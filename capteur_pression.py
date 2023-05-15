281f25031cb812f3e2d29941dbe4b10272da4273
Nassim <75885920+Hikarinass@users.noreply.github.com>
2023-05-15 15:40:33 +0200

    Add files via upload

--- capteur_pression.py
+++ capteur_pression.py
@@ -3,6 +3,9 @@ while True :
     valeur=pin2.read_analog()
     tension=valeur*300/1023
     pression=tension/2.3
-    print("P=",pression,"hPa")
-    print((pression))
-    sleep(2000)
+    pression=pression*100
+    distance=(pression-10000)/(997*9.81)
+    distance=distance*1000
+    print("Distance que parcourt le capteur=",distance,"Cm")
+    #print((cm))
+    sleep(1000)
